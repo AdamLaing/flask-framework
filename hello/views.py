3e62969e30595a3ff0e67a7e2d683c1c35b171ed
Adam Laing <adam.laing99@gmail.com>
2018-09-07 00:07:55 -0400

    Debugging

---
+++ hello/views.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, flash
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+
+DEBUG = True 
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+
+import requests, zipfile2, io
+import pandas as pd
+import quandl
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+
+quandl.ApiConfig.api_key = "gDuZE7V4QqDbup-gRfYy"
+#get APIs for tickers and data
+tickers = requests.get('https://www.quandl.com/api/v3/databases/WIKI/codes'
+                   '?api_key=gDuZE7V4QqDbup-gRfYy')
+
+tickers = zipfile2.ZipFile(io.BytesIO(tickers.content))
+tickers.extractall()
+tickers = pd.read_csv(zipfile2.ZipFile.namelist(tickers)[0], header=None,
+                      usecols=[0])
+tickers = tickers[0].astype('str')
+tickers = tickers.str.replace('WIKI/', '').tolist()
+
+class ReusableForm(Form):
+    name = TextField('Ticker:', validators=[validators.required()])
+ 
+
+@manage.route('/')
+def index():
+  return render_template('index.html')
+
+@manage.route('/close', methods=('GET', 'POST'))
+def close():
+    form = ReusableForm(request.form)
+
+    if request.method == 'POST':
+        tick = request.form['name']
+        error = None
+        
+        if not tick:
+            error = 'Please enter a ticker.'
+
+        elif tick not in tickers:
+            error = 'Company not found. Try another Ticker. Please use capital letters.'
+
+
+        if error is None:
+            data = quandl.get_table('WIKI/PRICES',
+                        qopts = { 'columns': ['ticker', 'date', 'close'] },
+                        ticker = [tick],
+                        date = { 'gte': '2018-01-01', 'lte': '2018-01-31' })
+            p = figure(plot_width=400, plot_height=400, x_axis_type='datetime')
+  
+            # add a line renderer
+            p.line(data['date'], data['close'], line_width=2)
+  
+            script, div = components(p)
+            return render_template('close.html', div=div, script=script, form=form)
+
+        flash(error)
+    return render_template('close.html', form=form)
+    
+if __name__ == '__main__':
+  app.run(port=33507)
--- manage.py
+++ manage.py
@@ -1,69 +1,10 @@
-from flask import Flask, render_template, request, redirect, flash
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+#!/usr/bin/env python
+import os
+import sys
 
-DEBUG = True 
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
+    from django.core.management import execute_from_command_line
 
-import requests, zipfile2, io
-import pandas as pd
-import quandl
-from bokeh.plotting import figure
-from bokeh.resources import CDN
-from bokeh.embed import components
-
-quandl.ApiConfig.api_key = "gDuZE7V4QqDbup-gRfYy"
-#get APIs for tickers and data
-tickers = requests.get('https://www.quandl.com/api/v3/databases/WIKI/codes'
-                   '?api_key=gDuZE7V4QqDbup-gRfYy')
-
-tickers = zipfile2.ZipFile(io.BytesIO(tickers.content))
-tickers.extractall()
-tickers = pd.read_csv(zipfile2.ZipFile.namelist(tickers)[0], header=None,
-                      usecols=[0])
-tickers = tickers[0].astype('str')
-tickers = tickers.str.replace('WIKI/', '').tolist()
-
-class ReusableForm(Form):
-    name = TextField('Ticker:', validators=[validators.required()])
- 
-
-@manage.route('/')
-def index():
-  return render_template('index.html')
-
-@manage.route('/close', methods=('GET', 'POST'))
-def close():
-    form = ReusableForm(request.form)
-
-    if request.method == 'POST':
-        tick = request.form['name']
-        error = None
-        
-        if not tick:
-            error = 'Please enter a ticker.'
-
-        elif tick not in tickers:
-            error = 'Company not found. Try another Ticker. Please use capital letters.'
-
-
-        if error is None:
-            data = quandl.get_table('WIKI/PRICES',
-                        qopts = { 'columns': ['ticker', 'date', 'close'] },
-                        ticker = [tick],
-                        date = { 'gte': '2018-01-01', 'lte': '2018-01-31' })
-            p = figure(plot_width=400, plot_height=400, x_axis_type='datetime')
-  
-            # add a line renderer
-            p.line(data['date'], data['close'], line_width=2)
-  
-            script, div = components(p)
-            return render_template('close.html', div=div, script=script, form=form)
-
-        flash(error)
-    return render_template('close.html', form=form)
-    
-if __name__ == '__main__':
-  app.run(port=33507)
+    execute_from_command_line(sys.argv)
